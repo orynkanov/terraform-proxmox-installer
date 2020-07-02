d388fe6692b4c3a31a824b21196a3960cd2865a1
orynkanov <orynkanov@gmail.com>
2020-07-02 16:15:56 +0600

    show terraform version for create directory ~/terraform.d

--- terraform-proxmox-installer.sh
+++ terraform-proxmox-installer.sh
@@ -9,6 +9,7 @@ cd /opt || exit 1
 if [[ ! -f /usr/local/bin/terraform ]]; then
     wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip
     unzip terraform_*_linux_amd64.zip -d /usr/local/bin
+    terraform version
 fi
 
 git clone https://github.com/Telmate/terraform-provider-proxmox.git
